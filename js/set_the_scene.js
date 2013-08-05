7b7053b6322a0567258f936af1be2645f915d305
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-08-04 22:25:16 -0400

    add the initial particles

--- js/set_the_scene.js
+++ js/set_the_scene.js
@@ -27,9 +27,9 @@ function init_scene() {
   // add the camera
 
   camera = new THREE.PerspectiveCamera( 60, width / height, 0.01, 1e10 );
-  camera.position.x = 50;
-  camera.position.y = 10;
-  camera.position.z = -500; // in millimeters
+  camera.position.x = -1000;
+  camera.position.y = 1000;
+  camera.position.z = -1000; // in millimeters
   
   // add the controls to look around the scene
 
@@ -72,21 +72,24 @@ function init_scene() {
   m3.makeRotationZ( gamma );
   
   // 1 sq. meter
-  var x_floor_color = new THREE.MeshBasicMaterial( { color:0xff0000, side: THREE.DoubleSide } );
-  var x_floor_geom = new THREE.PlaneGeometry(1000, 1000, 10, 10)
+  var x_floor_color  = new THREE.MeshBasicMaterial( 
+    { color:0xff0000, side: THREE.DoubleSide } );
+  var x_floor_geom   = new THREE.PlaneGeometry(1000, 1000, 10, 10)
   x_floor_geom.applyMatrix(m2); // rotate around y
-  var x_floor = new THREE.Mesh( x_floor_geom, x_floor_color );
+  var x_floor        = new THREE.Mesh( x_floor_geom, x_floor_color );
   x_floor.position.x = 500;
   
-  var y_floor_color = new THREE.MeshBasicMaterial( { color:0x00ff00, side: THREE.DoubleSide } );
-  var y_floor_geom = new THREE.PlaneGeometry(1000, 1000, 10, 10)
-  var y_floor = new THREE.Mesh( y_floor_geom, y_floor_color );
+  var y_floor_color  = new THREE.MeshBasicMaterial( 
+    { color:0x00ff00, side: THREE.DoubleSide } );
+  var y_floor_geom   = new THREE.PlaneGeometry(1000, 1000, 10, 10)
+  var y_floor        = new THREE.Mesh( y_floor_geom, y_floor_color );
   y_floor.applyMatrix(m1);
   y_floor.position.y = -500;
   
-  var z_floor_color = new THREE.MeshBasicMaterial( { color:0x0000ff, side: THREE.DoubleSide } );
-  var z_floor_geom = new THREE.PlaneGeometry(1000, 1000, 10, 10)
-  var z_floor = new THREE.Mesh( z_floor_geom, z_floor_color );
+  var z_floor_color  = new THREE.MeshBasicMaterial( 
+    { color:0x0000ff, side: THREE.DoubleSide } );
+  var z_floor_geom   = new THREE.PlaneGeometry(1000, 1000, 10, 10)
+  var z_floor        = new THREE.Mesh( z_floor_geom, z_floor_color );
   //z_floor.applyMatrix(m3);
   z_floor.position.z = 500;
   
@@ -98,14 +101,14 @@ function init_scene() {
 
   // stl files
   
-  var material = new THREE.MeshLambertMaterial( { color:0xffffff, side: THREE.DoubleSide } );
+  var stl_material = new THREE.MeshLambertMaterial( { color:0xffffff, side: THREE.DoubleSide } );
 
   var loader = new THREE.STLLoader();
   loader.addEventListener( 'load', function ( event ) {
 
-    var geometry = event.content;
+    var stl_geometry = event.content;
 
-    var mesh = new THREE.Mesh( geometry, material );
+    var mesh = new THREE.Mesh( stl_geometry, stl_material );
     mesh.position.setY( - 0.09 );
     scene.add( mesh );
 
@@ -113,6 +116,61 @@ function init_scene() {
   loader.load( "models/LEFT_ARM.stl" );
   loader.load( "models/LEFT_ELBOW.stl" );
   loader.load( "models/LEFT_GRIPPER.stl" );
+  
+  // particles from the mesh at first
+  var nparticles = fr_width * fr_height;
+  // TODO: Ensure npartciles does not exceed 65000
+  // TODO: Use the index attribute to overcome the limit
+  // as documented in buffer triangles
+	var geometry = new THREE.BufferGeometry();
+	geometry.attributes = {
+		position: {
+			itemSize: 3,
+			array: new Float32Array( nparticles * 3 )
+		},
+		color: {
+			itemSize: 3,
+			array: new Float32Array( nparticles * 3 )
+		}
+	} // geom attr
+  
+  // default (random) particle positions/colors
+  var positions = geometry.attributes.position.array;
+  var colors = geometry.attributes.color.array;
+  var color = new THREE.Color();
+  var n = 1000, n2 = n / 2; // particles spread in the cube
+  for ( var i = 0; i < positions.length; i += 3 ) {
+
+    // positions
+
+    var x = Math.random() * n - n2;
+    var y = Math.random() * n - n2;
+    var z = Math.random() * n - n2;
+
+    positions[ i ]     = x;
+    positions[ i + 1 ] = y;
+    positions[ i + 2 ] = z;
+
+    // colors
+
+    var vx = ( x / n ) + 0.5;
+    var vy = ( y / n ) + 0.5;
+    var vz = ( z / n ) + 0.5;
+
+    color.setRGB( vx, vy, vz );
+
+    colors[ i ]     = color.r;
+    colors[ i + 1 ] = color.g;
+    colors[ i + 2 ] = color.b;
+
+  }
+
+  geometry.computeBoundingSphere();
+  
+  var material = new THREE.ParticleBasicMaterial( { size: 15, vertexColors: true } );
+
+  particleSystem = new THREE.ParticleSystem( geometry, material );
+  scene.add( particleSystem );
 
   // renderer
 
