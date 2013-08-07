209e0dc9643d02b199100075c5263d891b5215cb
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-08-06 22:49:03 -0400

    Better gui

--- zmq_to_ws.js
+++ zmq_to_ws.js
@@ -6,10 +6,11 @@
 /**
 * Load configuration values
 */
-var zmq_send_ch = ['from_operator', 'to_mesh'];
-var zmq_recv_ch = ['to_operator',   'from_mesh'];
-var ws_names    = ['operator',      'mesh'];
-var ws_ports    = [9001,            9002];
+var zmq_send_ch = ['from_operator', 'to_mesh',   'to_rgbd'  ];
+var zmq_recv_ch = ['to_operator',   'from_mesh', 'from_rgbd'];
+var ws_names    = ['operator',      'mesh',      'rgbd'     ];
+var ws_ports    = [9001,            9002,        9003       ];
+
 var ws_servers  = new Array(ws_ports.length);
 /* Only allow one client (the latest one) */
 var ws_clients  = new Array(ws_ports.length);
@@ -19,11 +20,9 @@ for(var w=0;w<ws_names.length;w++){
   ws_name_reverse[ws_names[w]] = w;
   ws_clients[w] = null;
 }
-var zmq_receivers = new Array(ws_names.length);
 /* Only allow one client (the latest one) */
+var zmq_receivers = new Array(ws_names.length);
 var zmq_senders   = new Array(ws_names.length);
-
-/* NOTE: Everything after this is the same for each port forward */
 
 /**
 * Global state variables
@@ -38,7 +37,6 @@ var zmq = require('zmq');
 var mp  = require('msgpack');
 var WebSocketServer = require('ws').Server;
 
-
 /***************
 * WebSocket user request handling
 */
@@ -49,30 +47,31 @@ var ws_error = function(e){
 for( var w=0; w<ws_servers.length; w++) {
   
   /* Initialize the server */
+  console.log(ws_ports[w]);
   var wss = new WebSocketServer({port: ws_ports[w]});
+  wss.id = w;
   /* Save which socket this is */
-  var ws_id = w;
   /* Get a connection from a user */
   wss.on('connection', function(ws) {
-    
+    ws.id = this.id;
     /* Web Browser Message */
     ws.on('message', function(message) {
       /* Accept JSON broswer commands and no binary */
       var cmd = JSON.parse(message);
-      console.log('Browser ' + cmd.type + ' | ', cmd);
+      console.log('Browser | ', cmd);
       /* Pack as MessagePack for use by the robot, and send on the socket */
       var mp_cmd = mp.pack(cmd);
-      console.log('Forwarding to '+zmq_send_ch[ws_id]+' '+mp_cmd.length+' bytes.')
-      zmq_senders[ws_id].send(mp_cmd);
+      console.log('Forwarding to '+zmq_send_ch[ws.id]+' '+mp_cmd.length+' bytes.')
+      zmq_senders[ws.id].send(mp_cmd);
     });
     
     /* Save this web socket connection */
     ws_clients[ws_id] = ws;
     
-    console.log('User is connected to',ws_names[ws_id]);
+    console.log('User is connected to',this.id);
     
   }); // on connection
-  
+  console.log('ws',ws_id)
   ws_servers[w] = wss;
   
 } // for loop
