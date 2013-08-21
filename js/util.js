3344bbce046c280d085044ec6f368320d8547faf
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-08-20 21:32:17 -0400

    reorg

---
+++ js/util.js
@@ -0,0 +1,6 @@
+// Find the right host
+var host = window.document.location.host.replace(/:.*/, '');
+if( host.length==0 ){ host = "localhost"; }
+
+// Compatibility layer for URL
+var URL = window.URL || window.webkitURL;
