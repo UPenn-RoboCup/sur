5523a1d0514ed14b49cc7d4fc3b39db3ed3aaba1
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-09-11 20:25:17 -0400

    can now see the webgl

--- js/three_scene.js
+++ js/three_scene.js
@@ -1,7 +1,7 @@
 // Setup the THREE scene
 var scene, renderer, camera, stats;
 document.addEventListener( "DOMContentLoaded", function(){
-  var CANVAS_WIDTH = 200, CANVAS_HEIGHT = 200;
+  var CANVAS_WIDTH = 400, CANVAS_HEIGHT = 400;
   var container = document.getElementById( 'scene_container' );
   if(container===undefined){return;}
 
@@ -9,19 +9,50 @@ document.addEventListener( "DOMContentLoaded", function(){
   scene = new THREE.Scene();
 
   // add the camera
-  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1e6 );
+  camera = new THREE.PerspectiveCamera( 60, CANVAS_WIDTH / CANVAS_HEIGHT, 0.1, 1e6 );
+  camera.position.z = 2;
 
   // add light
-
   var dirLight = new THREE.DirectionalLight( 0xffffff );
   dirLight.position.set( 0, 0, 10 ).normalize();
-
   camera.add( dirLight );
   camera.add( dirLight.target );
+  camera.lookAt(new THREE.Vector3(0,0,0))
   
   // add the camera to the scene
-  
   scene.add( camera );
+
+
+
+///////////////
+///////////////
+var sphereMaterial =
+  new THREE.MeshLambertMaterial(
+    {
+      color: 0xFF0000
+    });
+// set up the sphere vars
+var radius = 1,
+    segments = 16,
+    rings = 16;
+
+// create a new mesh with
+// sphere geometry - we will cover
+// the sphereMaterial next!
+var sphere = new THREE.Mesh(
+
+  new THREE.SphereGeometry(
+    radius,
+    segments,
+    rings),
+
+  sphereMaterial);
+scene.add(sphere);
+///////////////
+///////////////
+
+
+
 
   // fps stats
   
@@ -29,7 +60,7 @@ document.addEventListener( "DOMContentLoaded", function(){
 
   // re-add?
   renderer = new THREE.WebGLRenderer( { antialias: false } );
-  renderer.setClearColor( 0x000000, 1 );
+  renderer.setClearColor( 0x005500, 1 );
   renderer.setSize( CANVAS_WIDTH, CANVAS_HEIGHT );
   // Add to the container
   container.appendChild( renderer.domElement );
