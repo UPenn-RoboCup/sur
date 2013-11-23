d246969014e4575be8a3ec5bd6d2a829246621f2
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-11-22 23:36:05 -0500

    largevalve model

---
+++ js/largevalve.js
@@ -0,0 +1,182 @@
+(function(ctx){
+  
+  // Function to hold methods
+  function LargeValve(){}
+  
+  //////////////
+  // RPC URLs //
+  //////////////
+  var rpc_url = rest_root+'/m/hcm/largevalve/model'
+  
+  /////////////////////
+  // Mesh definition //
+  /////////////////////
+  // make the wheel (some params known a priori)
+  var off_ground = 1010;
+  var radius0  = 130;
+  var ind_sz   = 10;
+  var tube_dia = 25/2;
+  var item_geo = new THREE.TorusGeometry(radius0, tube_dia, 8, 20 );
+  var item_mat = new THREE.MeshLambertMaterial({color: 0x5C5858});
+  // Vertical spoke
+  var vert_mesh = new THREE.Mesh(new THREE.CylinderGeometry(tube_dia/2,tube_dia/2,2*radius0,12,1));
+  // Horizontal Spoke
+  var horiz_mesh = new THREE.Mesh(new THREE.CylinderGeometry(tube_dia/2,tube_dia/2,2*radius0,12,1));
+  horiz_mesh.rotation.set(0, 0, Math.PI/2);
+  // Full mesh
+  THREE.GeometryUtils.merge( item_geo, vert_mesh );
+  THREE.GeometryUtils.merge( item_geo, horiz_mesh );
+  var item_mesh = new THREE.Mesh( item_geo, item_mat );
+  item_mesh.position.y = off_ground;
+  // Roll indicators should be a submesh
+  var start_mat  = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
+  var start_path = new THREE.Path();
+  start_path.fromPoints([
+    new THREE.Vector2(0,0), // tip of the arrow
+    new THREE.Vector2(-tube_dia,radius0-tube_dia),
+    new THREE.Vector2(0,radius0),
+    new THREE.Vector2(tube_dia,radius0-tube_dia),
+  ]);
+  var start_shape = start_path.toShapes();
+  var start_geo  = new THREE.ExtrudeGeometry(
+    start_shape, {amount: ind_sz}
+  );
+  var start_mesh  = new THREE.Mesh( start_geo, start_mat );
+  start_mesh.position.setZ(-ind_sz/2);
+  //
+  var stop_mat  = new THREE.MeshLambertMaterial({color: 0xFF0000});
+  var stop_path = new THREE.Path();
+  stop_path.fromPoints([
+    new THREE.Vector2(0,0), // tip of the arrow
+    new THREE.Vector2(-tube_dia,radius0-tube_dia),
+    new THREE.Vector2(0,radius0),
+    new THREE.Vector2(tube_dia,radius0-tube_dia),
+  ]);
+  var stop_shape = stop_path.toShapes();
+  var stop_geo  = new THREE.ExtrudeGeometry(
+    stop_shape, {amount: ind_sz}
+  );
+  var stop_mesh  = new THREE.Mesh( stop_geo, stop_mat );
+  stop_mesh.position.setZ(-ind_sz/2);
+  stop_mesh.rotation.set(0, 0, Math.PI/2);
+  // append to the valve
+  item_mesh.add(start_mesh);
+  item_mesh.add(stop_mesh);
+  
+  var mod_mesh = item_mesh;
+  
+  //////////////////////
+  // Model converters //
+  //////////////////////
+  var three_to_model = function(){
+    var roll_start = start_mesh.rotation.z;
+    var roll_end = stop_mesh.rotation.z;
+    var p = Transform.three_to_torso(item_mesh.position,Robot);
+    p.push(radius0/1000);
+    p.push(roll_start);
+    p.push(roll_end);
+    return p;
+  }
+  var model_to_three = function(model){
+    // {pos(3) roll_start roll_end}
+    var p = Transform.torso_to_three(model[0],model[1],model[2],Robot);
+    item_mesh.position.fromArray(p);
+    
+    // apply some yaw based upon our pose
+    var q_pose = (new THREE.Quaternion()).setFromAxisAngle(
+      (new THREE.Vector3(0,1,0)), Robot.pa );
+    var q_item = (new THREE.Quaternion()).setFromEuler(item_mesh.rotation);
+    var q_yaw = (new THREE.Quaternion()).multiplyQuaternions(q_pose,q_item);
+    item_mesh.rotation.setFromQuaternion(q_yaw);
+    
+    var roll_start = model[3];
+    var roll_end   = model[4];
+    start_mesh.rotation.set(0, 0, roll_start);
+    stop_mesh.rotation.set(0, 0, roll_end);
+  }
+  
+  /////////////////////////////
+  // Object manipulation API //
+  /////////////////////////////
+  LargeValve.select = function(p,r){
+    
+    // Set the position from the double click
+    item_mesh.position.copy(p);
+
+    // Re-render
+    World.render();
+
+  }
+  // modification loop
+  LargeValve.loop = function(tcontrol){
+    if(Manipulation.is_mod==false){
+      // Just reload the model from the robot
+      LargeValve.clear();
+      return;
+    }
+    // cycle the tcontrol
+    if(mod_mesh===item_mesh){
+      tcontrol.detach( item_mesh );
+      mod_mesh = start_mesh;
+    } else if(mod_mesh===start_mesh){
+      tcontrol.detach( start_mesh );
+      mod_mesh = stop_mesh;
+    } else {
+      tcontrol.detach( stop_mesh );
+      mod_mesh = item_mesh;
+    }
+    tcontrol.attach( mod_mesh );
+    tcontrol.update();
+    World.render();
+
+  }
+  // send data to the robot
+  LargeValve.send = function(){
+    var model = three_to_model();
+    qwest.post( rpc_url, {val:JSON.stringify(model)} );
+  }
+  // clear the item
+  LargeValve.clear = function(){
+    // Get the model from the robot (could be pesky...?)
+    qwest.get( rpc_url,{},{},function(){
+      // Use a 1 second timeout for the XHR2 request for getting the model
+      this.timeout = 1000; // ms
+    })
+    .success(function(model){
+      model_to_three(model);
+      mod_mesh = item_mesh;
+      World.render();
+    })
+  }
+  // enter
+  LargeValve.init = function(){
+    World.add(item_mesh);
+  }
+  // exit
+  LargeValve.deinit = function(){
+    World.remove(item_mesh);
+  }
+  LargeValve.get_mesh = function(){
+    return mod_mesh;
+  }
+  LargeValve.mod_callback = function(){
+    // Retain the same angles
+    item_mesh.rotation.x = 0;
+    item_mesh.rotation.y = 0;
+    start_mesh.rotation.x = 0;
+    start_mesh.rotation.y = 0;
+    stop_mesh.rotation.x = 0;
+    stop_mesh.rotation.y = 0;
+    // Retain the same height
+    item_mesh.position.y = off_ground;
+  }
+
+  /////////////////////////
+  // Metadata and Export //
+  /////////////////////////
+  LargeValve.item_name = 'Big Valve';
+  LargeValve.grab_evt = 'smallvalvegrab';
+  // export
+	ctx.LargeValve = LargeValve;
+
+})(this);
