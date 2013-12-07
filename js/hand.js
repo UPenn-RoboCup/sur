4c357663af188d0675d14adaa1f6bb1e37f0a38c
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-12-06 21:39:40 -0500

    better hand api

--- js/hand.js
+++ js/hand.js
@@ -6,131 +6,144 @@
   //////////////
   // RPC URLs //
   //////////////
-  var rpc_url_lget = rest_root+'/m/hcm/hands/left_tr_target'
-  var rpc_url_lset = rest_root+'/m/hcm/hands/left_tr_target'
+  var rpc_url_lget = rest_root+'/m/hcm/hands/left_tr';
+  var rpc_url_lset = rest_root+'/m/hcm/hands/left_tr_target';
   //
-  var rpc_url_rget = rest_root+'/m/hcm/hands/right_tr_target'
-  var rpc_url_rset = rest_root+'/m/hcm/hands/right_tr_target'
+  var rpc_url_rget = rest_root+'/m/hcm/hands/right_tr';
+  var rpc_url_rset = rest_root+'/m/hcm/hands/right_tr_target';
   
   /////////////////////
   // Mesh definition //
   /////////////////////
-  var item_mat  = new THREE.MeshLambertMaterial({color: 0xFF0000});
-  var item_path = new THREE.Path();
-  item_path.fromPoints([
-    new THREE.Vector2(0,0), // tip of the hand
-    new THREE.Vector2(20,0),
-    new THREE.Vector2(20,-100),
-    new THREE.Vector2(10,-100),
-    new THREE.Vector2(10,-10),
-    new THREE.Vector2(0,-10),
-  ]);
-  var item_shape = item_path.toShapes();
-  var item_geo  = new THREE.ExtrudeGeometry(
-    item_shape, {amount:20}
-  );
-  var item_mesh  = new THREE.Mesh( item_geo, item_mat );
-  item_mesh.rotation.set( Math.PI/2, 0, 0 );
-  //
-  var item_mesh_default_rot = new THREE.Quaternion();
-  var item_mesh_default_rot_inv = new THREE.Quaternion();
-  item_mesh_default_rot.setFromEuler(item_mesh.rotation);
-  item_mesh_default_rot_inv.copy(item_mesh_default_rot).inverse();
+  
+  // Left Hand
+  var left_mat  = new THREE.MeshLambertMaterial({color: 0xFF0000});
+  var left_geo  = new THREE.TorusGeometry(40, 25.4, 8, 6, Math.PI);
+  var left_mesh = new THREE.Mesh( left_geo, left_mat );
   
   // Right Hand
+  var right_mat  = new THREE.MeshLambertMaterial({color: 0xFFFF00});
+  var right_geo  = new THREE.TorusGeometry(40, 25.4, 8, 6, Math.PI);
+  var right_mesh = new THREE.Mesh( right_geo, right_mat );
   
+  // Default mesh
+  var mod_mesh = left_mesh;
+  var cur_hand = 'left';
 
   //////////////////////
   // Model converters //
   //////////////////////
-  var model_to_three = function(tr){
+  var model_to_three = function(tr,hand){
     // Make into easy formats
     var pos_array = Transform.torso_to_three(tr[0],tr[1],tr[2],Robot);
     var rot_q = (new THREE.Quaternion()).setFromEuler(new THREE.Euler(tr[4],tr[5],tr[3]));
     
-    // Make the rotation
-    var full_rot = new THREE.Quaternion();
-    full_rot.multiplyQuaternions(rot_q,item_mesh_default_rot);
-    
     // Incorporate pose
     var q_pose = (new THREE.Quaternion()).setFromAxisAngle(
       (new THREE.Vector3(0,1,0)), Robot.pa );
-    var full_rot2 = new THREE.Quaternion();
-    full_rot2.multiplyQuaternions(q_pose,full_rot);
+    var full_rot = new THREE.Quaternion();
+    full_rot.multiplyQuaternions(q_pose,rot_q);
 
-    // Update the mesh
-    item_mesh.rotation.setFromQuaternion(full_rot2);
-    item_mesh.position.fromArray(pos_array);
+    // Update the correct mesh
+    if(hand=='left'){
+      left_mesh.rotation.setFromQuaternion(full_rot);
+      left_mesh.position.fromArray(pos_array);
+    } else {
+      right_mesh.rotation.setFromQuaternion(full_rot);
+      right_mesh.position.fromArray(pos_array);
+    }
   }
-  var three_to_model = function(){
-    var q = (new THREE.Quaternion()).setFromEuler(item_mesh.rotation);
-    
+  var three_to_model = function(hand){
+    // Grab the item model from the correct mesh
+    var q, tr;
+    if(hand=='left'){
+      q = (new THREE.Quaternion()).setFromEuler(left_mesh.rotation);
+      tr = Transform.three_to_torso(left_mesh.position,Robot);
+    } else {
+      q = (new THREE.Quaternion()).setFromEuler(right_mesh.rotation);
+      tr = Transform.three_to_torso(right_mesh.position,Robot);
+    }
     // Undo the pose orientation
     var q_pose = (new THREE.Quaternion()).setFromAxisAngle(
       (new THREE.Vector3(0,1,0)), Robot.pa );
     var q_pose_inv = q_pose.inverse();
-    
     // Relative
     var q_rel = (new THREE.Quaternion()).multiplyQuaternions(q_pose_inv,q);
-    
-    // Put into the robot frame
-    var q_robot = (new THREE.Quaternion())
-    .multiplyQuaternions(q_rel,item_mesh_default_rot_inv);
-    
-    var rpy = (new THREE.Euler()).setFromQuaternion(q_robot);
-    var tr = Transform.three_to_torso(item_mesh.position,Robot)
-    tr.push(rpy.z, rpy.x, rpy.y)
+    // RPY
+    var rpy = (new THREE.Euler()).setFromQuaternion(q_rel);
+    tr.push(rpy.z, rpy.x, rpy.y);
     return tr;
   }
-  
   
   /////////////////////////////
   // Object manipulation API //
   /////////////////////////////
   // intersection handler
   Hand.select = function(p,r){
-    // Set the position
-    item_mesh.position.copy(p);
+    // Set the position for the current mesh
+    mod_mesh.position.copy(p);
   }
   // reset the hand
   // loop modify handles
   Hand.loop = function(tcontrol){
-    qwest.get( rpc_url_rget ).success(function(model){
-      // Convert the position to THREEjs
-      model_to_three(model);
-      tcontrol.update();
-    });
+    if(Manipulation.is_mod==false){
+      // Reload the models
+      qwest.get( rpc_url_lget ).success(function(lmodel){
+        // Convert the position to THREEjs
+        model_to_three(lmodel,'left');
+        qwest.get( rpc_url_rget ).success(function(rmodel){
+          // Convert the position to THREEjs
+          model_to_three(rmodel,'right');
+        });
+      });     
+      return;
+    }
+    // Switch hands
+    if(cur_hand=='left') {
+      tcontrol.detach( left_mesh );
+      cur_hand = 'right';
+      mod_mesh = right_mesh;
+    } else {
+      tcontrol.detach( right_mesh );
+      cur_hand = 'left';
+      mod_mesh = left_mesh;
+    }
+    tcontrol.attach( mod_mesh );
+    tcontrol.update();
   }
   // enter stage
   Hand.init = function(tcontrol){
-    // Grab the current transform
-    qwest.get( rpc_url_rget ).success(function(model){
+    // Grab the current transforms
+    qwest.get( rpc_url_lget ).success(function(lmodel){
       // Convert the position to THREEjs
-      model_to_three(model);
-      // Must update the position of the transform controls
-      tcontrol.update();
+      model_to_three(lmodel,'left');
+      qwest.get( rpc_url_rget ).success(function(rmodel){
+        // Convert the position to THREEjs
+        model_to_three(rmodel,'right');
+      });
     });
+
     // Add to the world
-    World.add(item_mesh);
-    // Re-render
-    
+    World.add(left_mesh);
+    World.add(right_mesh);
   }
   // exit stage
   Hand.deinit = function(){
-    World.remove(item_mesh);
+    World.remove(left_mesh);
+    World.remove(right_mesh);
   }
   // send to robot
   Hand.send = function(){
     // Acquire the model
     var model = three_to_model();
-    qwest.post( rpc_url_rset, {val:JSON.stringify(model)} );
+    //qwest.post( rpc_url_rset, {val:JSON.stringify(model)} );
   }
   Hand.mod_callback = function(){
     
   }
   // get the mesh
   Hand.get_mod_mesh = function(){
-    return item_mesh;
+    return mod_mesh;
   }
   
   Hand.add_buttons = function(holder){
