3aaf851c0dcc70b84433e4022a67cff09ee23f43
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-12-10 01:38:35 -0500

    fix hose wp

--- models/hose.js
+++ models/hose.js
@@ -64,13 +64,13 @@
   // Adjust the waypoint to the *perfect* position
   var wp_callback = function(){
     // Grab the (global) orientation of the mesh
-    var pa = -1*item_mesh.rotation.y;
+    var pa = item_mesh.rotation.y;
     // Acquire the position of the tip:
     var p = (new THREE.Vector3()).copy(item_mesh.position);
     
     // Make the global offset from the object    
-    var dx = offset.x*Math.cos(pa) + offset.y*Math.sin(pa);
-    var dy = offset.y*Math.cos(pa) - offset.x*Math.sin(pa);
+    var dx = offset.x*Math.cos(pa) - offset.y*Math.sin(pa);
+    var dy = offset.y*Math.cos(pa) + offset.x*Math.sin(pa);
 
     // Change the THREE coordinates of the desired waypoint
     p.x -= dy;
