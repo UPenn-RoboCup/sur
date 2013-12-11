1848817d005f59d4a1dc30d9bac3a05afbbcd6be
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-12-10 20:02:53 -0500

    fix bar valve

--- models/barvalve.js
+++ models/barvalve.js
@@ -6,7 +6,11 @@
   //////////////
   // RPC URLs //
   //////////////
-  var rpc_url = rest_root+'/m/hcm/barvalve/model'
+  var rpc_url = rest_root+'/m/hcm/barvalve/model';
+  
+  // Relative waypoint offset in ROBOT coordinates
+  // but with THREE scale (mm)
+  var offset = new THREE.Vector2(600,300);
   
   /////////////////////
   // Mesh definition //
@@ -18,18 +22,37 @@
   var tube_rad   = 15;
   var item_mesh = new THREE.Mesh(
     new THREE.SphereGeometry(tube_rad),
-    new THREE.MeshLambertMaterial({color: 0xAA1111})
+    new THREE.MeshLambertMaterial({color: 0xAAAAAA})
   );
   // Horizontal Spoke
   var horiz_mesh = new THREE.Mesh(
     new THREE.CylinderGeometry(tube_rad,tube_rad,radius0,12,1),
-    new THREE.MeshLambertMaterial({color: 0xEE2222})
+    new THREE.MeshLambertMaterial({color: 0xAAAAAA})
   );
   horiz_mesh.rotation.set(0, 0, Math.PI/2);
   horiz_mesh.position.x = radius0/2;
   
   // append to the valve
   item_mesh.add(horiz_mesh);
+  
+  //
+  var stop_mat  = new THREE.MeshLambertMaterial({color: 0xFF0000});
+  var stop_path = new THREE.Path();
+  stop_path.fromPoints([
+    new THREE.Vector2(0,0), // tip of the arrow
+    new THREE.Vector2(-tube_rad,radius0-tube_rad),
+    new THREE.Vector2(0,radius0),
+    new THREE.Vector2(tube_rad,radius0-tube_rad),
+  ]);
+  var stop_shape = stop_path.toShapes();
+  var stop_geo  = new THREE.ExtrudeGeometry(
+    stop_shape, {amount: ind_sz}
+  );
+  var stop_mesh  = new THREE.Mesh( stop_geo, stop_mat );
+  stop_mesh.position.setZ(-ind_sz/2);
+  stop_mesh.rotation.set(0, 0, Math.PI/2);
+  // append to the valve
+  item_mesh.add(stop_mesh);
   
   var mod_mesh = item_mesh;
   
@@ -97,11 +120,7 @@
   }
   // modification loop
   BarValve.loop = function(tcontrol){
-    if(Manipulation.is_mod==false){
-      // Just reload the model from the robot
-      BarValve.clear();
-      return;
-    }
+    if(Manipulation.is_mod==false){return;}
     // cycle the tcontrol
     if(mod_mesh===item_mesh){
       tcontrol.detach( item_mesh );
@@ -123,9 +142,9 @@
     var model = three_to_model();
     qwest.post( rpc_url, {val:JSON.stringify(model)} );
   }
-  // clear the item
-  BarValve.clear = function(){
-    // Get the model from the robot (could be pesky...?)
+  // enter
+  BarValve.init = function(){
+    World.add(item_mesh);
     qwest.get( rpc_url,{},{},function(){
       // Use a 1 second timeout for the XHR2 request for getting the model
       this.timeout = 1000; // ms
@@ -133,32 +152,32 @@
     .success(function(model){
       model_to_three(model);
       mod_mesh = item_mesh;
-      
-    })
-  }
-  // enter
-  BarValve.init = function(){
-    World.add(item_mesh);
+    });
   }
   // exit
   BarValve.deinit = function(){
     World.remove(item_mesh);
   }
-  BarValve.get_mesh = function(){
+  BarValve.get_mod_mesh = function(){
     return mod_mesh;
   }
   BarValve.mod_callback = function(){
     // Retain the same angles
     item_mesh.rotation.x = 0;
     item_mesh.rotation.y = 0;
-    start_mesh.rotation.x = 0;
-    start_mesh.rotation.y = 0;
+    //
     stop_mesh.rotation.x = 0;
     stop_mesh.rotation.y = 0;
     // Retain the same height
     item_mesh.position.y = off_ground;
 
     wp_callback();
+  }
+  BarValve.special = function(dir){
+    // Move the roll
+    item_mesh.rotation.z += dir*.1;
+    wp_callback();
+    SmallValve.send();
   }
 
   /////////////////////////
