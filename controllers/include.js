a2d23c6364c58bfee4e351e3e2f323caf1149281
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-03-06 21:32:13 -0500

    adding the figures

--- controllers/include.js
+++ controllers/include.js
@@ -1,51 +1,53 @@
 // Useful (globally accessible) functions
-this.hostname = this.location.hostname;
-if (!this.hostname) {
-	this.hostname = 'localhost';
-	this.origin = 'http://localhost:8080';
-} else {
-	this.origin = this.location.origin;
-}
-this.fsm_url  = this.origin + '/s';
-this.body_url = this.origin + '/b';
-this.shm_url  = this.origin + '/m';
-// Override get/set
-this.so_url = this.shm_url + '/m/hcm/state/override';
-this.rpc_url_proceed = this.shm_url + '/m/hcm/state/proceed';
-// how far in the future is the operator (in seconds)?
-this.time_offset = 0.7;
-// http://macwright.org/presentations/dcjq/
-this.$ = function (x) {
-	"use strict";
-	return this.document.querySelectorAll(x);
-};
-this.clicker = function (id, fun) {
-	"use strict";
-	if (typeof id === 'string') {
-		var id_el = this.document.getElementById(id);
-		if (id_el === null) {
+(function(ctx){
+	ctx.hostname = ctx.location.hostname;
+	if (!ctx.hostname) {
+		ctx.hostname = 'localhost';
+		ctx.origin = 'http://localhost:8080';
+	} else {
+		ctx.origin = ctx.location.origin;
+	}
+	ctx.fsm_url  = ctx.origin + '/s';
+	ctx.body_url = ctx.origin + '/b';
+	ctx.shm_url  = ctx.origin + '/m';
+	// Override get/set
+	ctx.so_url = ctx.shm_url + '/m/hcm/state/override';
+	ctx.rpc_url_proceed = ctx.shm_url + '/m/hcm/state/proceed';
+	// how far in the future is the operator (in seconds)?
+	ctx.time_offset = 0.7;
+	// http://macwright.org/presentations/dcjq/
+	ctx.$ = function (x) {
+		"use strict";
+		return ctx.document.querySelectorAll(x);
+	};
+	ctx.clicker = function (id, fun) {
+		"use strict";
+		if (typeof id === 'string') {
+			var id_el = ctx.document.getElementById(id);
+			if (id_el === null) {
+				return false;
+			}
+			return ctx.Hammer(id_el).on('tap', fun);
+		}
+		if (id === null || id === undefined) {
 			return false;
 		}
-		return this.Hammer(id_el).on('tap', fun);
-	}
-	if (id === null || id === undefined) {
-		return false;
-	}
-	return this.Hammer(id).on('tap', fun);
-};
-this.unclicker = function (id, fun) {
-	"use strict";
-	if (typeof id === 'string') {
-		var id_el = this.document.getElementById(id);
-		if (id_el === null) {
+		return ctx.Hammer(id).on('tap', fun);
+	};
+	ctx.unclicker = function (id, fun) {
+		"use strict";
+		if (typeof id === 'string') {
+			var id_el = ctx.document.getElementById(id);
+			if (id_el === null) {
+				return false;
+			}
+			return ctx.Hammer(id_el).off('tap', fun);
+		}
+		if (id === null || id === undefined) {
 			return false;
 		}
-		return this.Hammer(id_el).off('tap', fun);
-	}
-	if (id === null || id === undefined) {
-		return false;
-	}
-	return this.Hammer(id).off('tap', fun);
-};
-this.DEG_TO_RAD = Math.PI / 180;
-this.RAD_TO_DEG = 180 / Math.PI;
+		return ctx.Hammer(id).off('tap', fun);
+	};
+	ctx.DEG_TO_RAD = Math.PI / 180;
+	ctx.RAD_TO_DEG = 180 / Math.PI;
+})(this);
