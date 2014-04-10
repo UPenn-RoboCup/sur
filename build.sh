31a41581fc2bd32840e698f7d5556492a29adc54
Karenqh <heqin@seas.upenn.edu>
2014-04-09 20:58:15 -0400

    Tested on linux machine

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+# Node.js from apt-get is too old. First do:
+# sudo add-apt-repository ppa:chris-lea/node.js
+# sudo apt-get update
+# sudo apt-get install nodejs
+# Check if /usr/local/bin/node exists, if not:
+# sudo ln -s /usr/bin/node /use/local/bin/node
 npm install msgpack ws zmq restify underscore peer
