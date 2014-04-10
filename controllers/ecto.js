579bf0f33dcd4b7947c279f076cae024f061bd50
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-04-10 12:32:59 +0200

    more events

--- controllers/ecto.js
+++ controllers/ecto.js
@@ -6,14 +6,42 @@ this.addEventListener("load", function () {
   var el = document.getElementsByTagName("body")[0],
 			port = 9064,
 			ws = new this.WebSocket('ws://' + this.hostname + ':' + port),
-			to = {};
+			beats = {},
+			BEAT_INTERVAL = 1000/5, // fps
+			trails = {},
+			trail_counts = {},
+			TRAIL_COUNT = 5,
+			TRAIL_INTERVAL = 500/TRAIL_COUNT;
 	
 	function heartbeat(){
-		this.ws.send(JSON.stringify({
+		var beater = {
 			t: Date.now(),
 			e: 'beat',
 			id: this.id
-		}));
+		}
+		this.ws.send(JSON.stringify(beater));
+	}
+
+	function trail(){
+		var id = this.id;
+		var cnt = trail_counts[id];
+		// Increment the count
+		trail_counts[id] = cnt + 1;
+		// Check if done the trail
+		if(cnt>TRAIL_COUNT){
+			clearInterval( trails[id] );
+			delete trails[id];
+			delete trail_counts[id];
+			return;
+		}
+		var trailer = {
+			t: Date.now(),
+			id: id,
+			e: 'trail',
+			cnt: cnt // cnt begins at 1
+		};
+		this.ws.send(JSON.stringify(trailer));
+		console.log(trailer);
 	}
 	
 	function procEvent(evt,name){
@@ -37,17 +65,26 @@ this.addEventListener("load", function () {
 			Clear the timeout for this id,
 			since an event happened
 			*/
-			if( to[id] !== undefined ){
-				clearInterval( to[id] );
+			if( beats[id] !== undefined ){
+				clearInterval( beats[id] );
 			}
 			/* If touch is dead, remove key so 
-				memory does not leak
+				memory does not leak. Leave a trail on stop
 			*/
-			if(name!=='start' && name!=='move'){
-				delete to[id];
-			} else {
-				// Set the timeout anew
-				to[id] = setInterval( heartbeat.bind({id: id, ws: ws}), 33 );
+			switch(name){
+					case 'start':
+					case 'move':
+						beats[id] = setInterval( heartbeat.bind({id: id, ws: ws}), BEAT_INTERVAL );
+						break;
+					case 'stop':
+						trails[id] = setInterval( trail.bind({id: id, ws: ws}), TRAIL_INTERVAL );
+						trail_counts[id] = 0;
+						break;
+					case 'cancel':
+					case 'leave':
+					default:
+						delete beats[id];
+						break;
 			}
 		}
 		ws.send(JSON.stringify(data));
