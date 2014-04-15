c89133a18607dea531f09d4ca28e1fa4e24eabcb
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-04-15 12:33:17 +0200

    remove some comments

--- controllers/thesis.js
+++ controllers/thesis.js
@@ -12,11 +12,13 @@ this.addEventListener("load", function () {
 		marked = this.marked,
 		MathJax = this.MathJax,
 		qwest = this.qwest;
-	
+
 	function setFigures() {
 		// Check if everything is available...
-		if (!svg_script_el || !tex_done) { return; }
-		window.console.log('here');
+		if (!svg_script_el || !tex_done) {
+			return;
+		}
+		//window.console.log('here');
 		// Grab the SVG elements inside this chapter
 		var svgs = document.querySelectorAll('#' + chapter_el.id + ' svg'),
 			n = svgs.length,
@@ -27,7 +29,7 @@ this.addEventListener("load", function () {
 		// Replace each figure with SVG if possible
 		for (i = 0; i < n; i = i + 1) {
 			s = svgs[i];
-			window.console.log('s', s);
+			//window.console.log('s', s);
 			// Fetch the correct drawing function, which is loaded externally
 			sf = snaps[s.id];
 			if (sf !== undefined) {
@@ -40,7 +42,7 @@ this.addEventListener("load", function () {
 			}
 		}
 	}
-	
+
 	function run_svgjs(snippet) {
 		svg_script_el = document.createElement("script");
 		svg_script_el.language = "javascript";
@@ -50,7 +52,7 @@ this.addEventListener("load", function () {
 		svg_script_el.text = snippet;
 		head_el.appendChild(svg_script_el);
 	}
-	
+
 	function runMarked() {
 		var marked_up = marked(chapter_el.innerHTML);
 		// Replace the double dash
@@ -63,7 +65,7 @@ this.addEventListener("load", function () {
 		// Set flag
 		tex_done = true;
 	} // runMarked
-	
+
 	function runMathJax(text) {
 		// Place the text into the element
 		chapter_el.innerHTML = text;
@@ -73,22 +75,26 @@ this.addEventListener("load", function () {
 		// Try fixing the figures always...?
 		MathJax.Hub.Queue(setFigures);
 	} // runMathJax
-	
+
 	function loadTex(e) {
 		// Load the text
-		qwest.get('/md/' + chapter_el.id, {}, {}, function () {this.responseType = 'text'; })
+		qwest.get('/md/' + chapter_el.id, {}, {}, function () {
+			this.responseType = 'text';
+		})
 			.success(runMathJax);
 		// Load the SVG replacement code
-		qwest.get('/snap/' + chapter_el.id + '.js', {}, {}, function () {this.responseType = 'text'; })
+		qwest.get('/snap/' + chapter_el.id + '.js', {}, {}, function () {
+			this.responseType = 'text';
+		})
 			.success(run_svgjs)
 			.complete(setFigures);
 	}
-	
+
 	// Set the div element as a chapter
 	chapter_el.classList.add("chapter");
 	// TODO: Which chapter to load
 	chapter_el.id = "swipe_radon";
 	// Only begin rendering when MathJax is loaded fully
 	MathJax.Hub.Register.StartupHook("End", loadTex);
-	
+
 }); // Page load listener
