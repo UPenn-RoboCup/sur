42d042f790a1d150f0f47f1525859df47b6e3fea
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-08-06 12:35:22 -0400

    Add bower, gulp, setting package.json for npm

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+# Server requirements, manage with node and package.json
 npm install msgpack ws zmq restify underscore peer nodelua
+# Client side JS Dev only
+npm install bower gulp gulp-concat main-bower-files gulp-uglify
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var mainBowerFiles = require('main-bower-files');
+
+gulp.task('concat_libs', function() {
+	return gulp.src(mainBowerFiles())
+		.pipe(sourcemaps.init())
+		.pipe(concat('ext.min.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('external'));
+});
+gulp.task('default', ['concat_libs']);
