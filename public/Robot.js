f81e8819ac631c8e6ec8e00e743e04c1a5255242
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-09-22 21:00:37 -0700

    movement

--- public/Robot.js
+++ public/Robot.js
@@ -63,8 +63,12 @@
 	});
 	function rotateServo(s, v) {
 		var mesh = s.mesh;
+		if (mesh === undefined || s.axel === undefined) {
+			//window.console.log(s, 'BAD');
+			return;
+		}
 		mesh.matrixWorldNeedsUpdate = true;
-		tmp_quat.setFromAxisAngle(s.axel, v).multiply(s.rot);
+		tmp_quat.setFromAxisAngle(s.axel, v + (s.offset || 0)).multiply(s.rot);
 		mesh.setRotationFromQuaternion(tmp_quat);
 	}
 	ws.onmessage = function (e) {
@@ -89,25 +93,21 @@
 		rot: new THREE.Quaternion()
 	};
 	parts.PELVIS = {
-		id: 29,
 		tr: new THREE.Vector3(0, -128, 0),
 		rot: new THREE.Quaternion()
 	};
 	parts.CHEST = {
-		id: 30,
 		tr: new THREE.Vector3(0, 170, 0),
 		rot: new THREE.Quaternion()
 	};
 	// Head
 	parts.NECK = {
-		id: 1,
 		parent: 'CHEST',
 		tr: new THREE.Vector3(0, 40, 0),
 		rot: new THREE.Quaternion(),
 		axel: new THREE.Vector3(0, 1, 0)
 	};
 	parts.CAM = {
-		id: 2,
 		parent: 'NECK',
 		tr: new THREE.Vector3(0, 111, 0),
 		rot: new THREE.Quaternion(),
@@ -115,136 +115,122 @@
 	};
 	// RArm
 	parts.RIGHT_SHOULDER_PITCH = {
-		id: 3,
 		parent: 'CHEST',
 		tr: new THREE.Vector3(STL_MOTOR_WIDTH - 234, 0, 0),
-		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 0, 1)), -Math.PI / 2)
+		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 0, 1)), -Math.PI / 2),
+		axel: new THREE.Vector3(1, 0, 0),
+		offset: -Math.PI / 2
 	};
 	parts.RIGHT_SHOULDER_ROLL = {
-		id: 4,
 		parent: 'RIGHT_SHOULDER_PITCH',
 		tr: new THREE.Vector3(0, -STL_MOTOR_WIDTH, STL_MOTOR_WIDTH / 2),
-		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 0, 1)), Math.PI / 2)
+		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 0, 1)), Math.PI / 2),
+		axel: new THREE.Vector3(0, 0, -1)
 	};
 	parts.RIGHT_ARM = {
-		id: 5,
 		parent: 'RIGHT_SHOULDER_ROLL',
 		tr: new THREE.Vector3(0, -STL_MOTOR_WIDTH / 2, -STL_MOTOR_WIDTH / 2),
-		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 1, 0)), Math.PI)
+		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 1, 0)), Math.PI),
+		axel: new THREE.Vector3(0, 1, 0)
 	};
 	parts.RIGHT_ELBOW = {
-		id: 6,
 		parent: 'RIGHT_ARM',
 		tr: new THREE.Vector3(0, -246 + STL_MOTOR_WIDTH / 2, 0),
 		rot: new THREE.Quaternion()
 	};
 	parts.RIGHT_WRIST = {
-		id: 7,
 		parent: 'RIGHT_ELBOW',
 		tr: new THREE.Vector3(0, -250, 0),
 		rot: new THREE.Quaternion()
 	};
 	// LArm
 	parts.LEFT_SHOULDER_PITCH = {
-		id: 3,
 		parent: 'CHEST',
 		tr: new THREE.Vector3(234 - STL_MOTOR_WIDTH, 0, 0),
-		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 0, 1)), Math.PI / 2)
+		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 0, 1)), Math.PI / 2),
+		axel: new THREE.Vector3(1, 0, 0),
+		offset: -Math.PI / 2
 	};
 	parts.LEFT_SHOULDER_ROLL = {
-		id: 4,
 		parent: 'LEFT_SHOULDER_PITCH',
 		tr: new THREE.Vector3(0, -STL_MOTOR_WIDTH, STL_MOTOR_WIDTH / 2),
-		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 0, 1)), -Math.PI / 2)
+		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 0, 1)), -Math.PI / 2),
+		axel: new THREE.Vector3(0, 0, 1)
 	};
 	parts.LEFT_ARM = {
-		id: 5,
 		parent: 'LEFT_SHOULDER_ROLL',
 		tr: new THREE.Vector3(0, -STL_MOTOR_WIDTH / 2, -STL_MOTOR_WIDTH / 2),
-		rot: new THREE.Quaternion()
+		rot: new THREE.Quaternion(),
+		axel: new THREE.Vector3(0, 1, 0)
 	};
 	parts.LEFT_ELBOW = {
-		id: 6,
 		parent: 'LEFT_ARM',
 		tr: new THREE.Vector3(0, -246 + STL_MOTOR_WIDTH / 2, 0),
 		rot: new THREE.Quaternion()
 	};
 	parts.LEFT_WRIST = {
-		id: 7,
 		parent: 'LEFT_ELBOW',
 		tr: new THREE.Vector3(0, -250, 0),
 		rot: new THREE.Quaternion()
 	};
 	// RLeg
 	parts.RIGHT_HIP_YAW = {
-		id: 7,
 		parent: 'PELVIS',
 		tr: new THREE.Vector3(-72, -16 - 64, 0),
 		rot: new THREE.Quaternion()
 	};
 	parts.RIGHT_HIP_ROLL = {
-		id: 7,
 		parent: 'RIGHT_HIP_YAW',
 		tr: new THREE.Vector3(0, -64, 0),
 		rot: new THREE.Quaternion()
 	};
 	parts.R_THIGH = {
-		id: 7,
 		parent: 'RIGHT_HIP_ROLL',
 		tr: new THREE.Vector3(),
 		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 1, 0)), Math.PI)
 	};
 	parts.R_LEG = {
-		id: 7,
 		parent: 'R_THIGH',
 		tr: new THREE.Vector3(0, -300, 0),
 		rot: new THREE.Quaternion()
 	};
 	parts.RIGHT_ANKLE = {
-		id: 7,
 		parent: 'R_LEG',
 		tr: new THREE.Vector3(0, -300, 0),
 		rot: (new THREE.Quaternion()).setFromAxisAngle((new THREE.Vector3(0, 1, 0)), Math.PI)
 	};
 	parts.RIGHT_FOOT = {
-		id: 7,
 		parent: 'RIGHT_ANKLE',
 		tr: new THREE.Vector3(),
 		rot: new THREE.Quaternion()
 	};
 	// LLeg
 	parts.LEFT_HIP_YAW = {
-		id: 7,
 		parent: 'PELVIS',
 		tr: new THREE.Vector3(72, -16 - 64, 0),
 		rot: new THREE.Quaternion()
 	};
 	parts.LEFT_HIP_ROLL = {
-		id: 7,
 		parent: 'LEFT_HIP_YAW',
 		tr: new THREE.Vector3(0, -64, 0),
 		rot: new THREE.Quaternion()
 	};
 	parts.L_THIGH = {
-		id: 7,
 		parent: 'LEFT_HIP_ROLL',
 		tr: new THREE.Vector3(),
 		rot: new THREE.Quaternion()
 	};
 	parts.L_LEG = {
-		id: 7,
 		parent: 'L_THIGH',
 		tr: new THREE.Vector3(0, -300, 0),
 		rot: new THREE.Quaternion()
 	};
 	parts.LEFT_ANKLE = {
-		id: 7,
 		parent: 'L_LEG',
 		tr: new THREE.Vector3(0, -300, 0),
 		rot: new THREE.Quaternion()
 	};
 	parts.LEFT_FOOT = {
-		id: 7,
 		parent: 'LEFT_ANKLE',
 		tr: new THREE.Vector3(),
 		rot: new THREE.Quaternion()
@@ -254,6 +240,15 @@
 	// Servo configuration
 	servos.push(parts.NECK);
 	servos.push(parts.CAM);
+	servos.push(parts.LEFT_SHOULDER_PITCH);
+	servos.push(parts.LEFT_SHOULDER_ROLL);
+	servos.push(parts.LEFT_ARM);
+	/*
+	servos.push(parts.RIGHT_SHOULDER_PITCH);
+	servos.push(parts.RIGHT_SHOULDER_ROLL);
+	servos.push(parts.RIGHT_ARM);
+	*/
+
 	// Load each chain
 	part_keys.forEach(function (part) {
 		loader.load('/stl/' + part + '.stl');
