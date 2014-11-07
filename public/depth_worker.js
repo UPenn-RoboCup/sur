c6312cfd8f5a8825b5c3a1037de49ade6aaac351
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-11-07 14:37:24 -0500

    Testing with the real sensor

--- public/depth_worker.js
+++ public/depth_worker.js
@@ -1,7 +1,7 @@
 var postMessage = this.postMessage;
-// Range in meters
-var MAX = 8,
-	MIN = 0.2,
+// Range in meters (or centimeters...)
+var MAX = 8000, //8,
+	MIN = 200, //0.2,
 	RANGE = MAX - MIN,
 	fourValue,
 	min = Math.min;
@@ -15,11 +15,11 @@ this.addEventListener('message', function (e) {
 		j;
 	for (i = 0, j = 0; i < len; i += 1, j += 4) {
 		img[j + 3] = 255;
-    if (z[i] < 0.2) {
+    if (z[i] < MIN) {
 			img[j] = 0;
 			img[j + 1] = 0;
 			img[j + 2] = 0;
-    } else if (z[i] > 8) {
+    } else if (z[i] > MAX) {
 			img[j] = 255;
 			img[j + 1] = 255;
 			img[j + 2] = 255;
--- public/kinect2_video.js
+++ public/kinect2_video.js
@@ -15,13 +15,13 @@
 		processing = false;
 
 	// Constant for the Kinect2
-	/*
 	depth_canvas.width = 512;
 	depth_canvas.height = 424;
-	*/
 	// Webots
+	/*
 	depth_canvas.width = 256;
 	depth_canvas.height = 212;
+	*/
 	depth_img_data = depth_ctx.getImageData(0, 0, depth_canvas.width, depth_canvas.height);
 
 	function toggle() {
@@ -70,6 +70,7 @@
 		// Paint the image back
 		depth_ctx.putImageData(depth_img_data, 0, 0);
 		processing = false;
+		//window.console.log(e.data.data);
 	}
 
 	// Add the camera view and append
