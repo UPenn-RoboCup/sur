b67f6b104966d424aec656232989b2233bb869b1
Stephen McGill <smcgill3@seas.upenn.edu>
2015-05-24 17:50:24 -0700

    Simplying network

--- public/fsm.js
+++ public/fsm.js
@@ -1,19 +1,34 @@
 (function () {
 	'use strict';
 
-	function send(){
+	function sendfsm(){
 		util.shm(this, true);
+	}
+
+	function sendshm(){
+		console.log(this);
+		util.shm(
+			'/shm/' + this.getAttribute("data-shm") +
+			'/' + this.getAttribute("data-segment") +
+			'/' + this.getAttribute("data-key"),
+			JSON.parse(this.getAttribute("data-value"))
+		);
 	}
 
 	function setup_buttons(fsm){
 		var allBtns = document.querySelectorAll('#'+fsm+' button');
-		console.log(allBtns);
 		for(var i = 0; i<allBtns.length; i+=1){
 			var btn = allBtns.item(i);
-			btn.addEventListener('click', send.bind(
-				'/fsm/'+fsm+'/'+btn.id
-			));
+			if(btn.parentNode.classList.contains("shm")){
+				console.log(btn);
+				btn.addEventListener('click', sendshm.bind(btn));
+			} else if(btn.parentNode.classList.contains("fsm")){
+				btn.addEventListener('click', sendfsm.bind(
+					'/fsm/'+fsm+'/'+btn.id
+				));
+			}
 		}
+
 	}
 	// Load everything
 	Promise.all([
@@ -24,10 +39,15 @@
 	}).then(function(view){
 		document.body = view;
 	}).then(function(){
-		var allFSMs = document.querySelectorAll('.button-group');
-		console.log(allFSMs);
-		for(var i = 0; i<allFSMs.length; i+=1){
-			var div = allFSMs.item(i);
+		var allFSMs = document.querySelectorAll('.fsm');
+		var i, div;
+		for(i = 0; i<allFSMs.length; i+=1){
+			div = allFSMs.item(i);
+			setup_buttons(div.id);
+		}
+		var allSHMs = document.querySelectorAll('.shm');
+		for(i = 0; i<allSHMs.length; i+=1){
+			div = allSHMs.item(i);
 			setup_buttons(div.id);
 		}
 	})
