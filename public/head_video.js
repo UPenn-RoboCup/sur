a7e5600bf4ef96fa71d7b210c4f23995b0efc5c6
Stephen McGill <smcgill3@seas.upenn.edu>
2015-05-26 00:07:00 -0700

    direct head motion from the camera feed

--- public/head_video.js
+++ public/head_video.js
@@ -32,10 +32,43 @@
 		lA_ctx.putImageData(lA_img_data, 0, 0);
 	}
 
+	var qHead = [0, 0];
+	function delta_head() {
+		qHead[0] += this[0] * util.DEG_TO_RAD;
+		qHead[1] += this[1] * util.DEG_TO_RAD;
+	}
+	function setup_keys(){
+		var listener = new keypress.Listener();
+		listener.simple_combo("w", delta_head.bind([0,-10]));
+		listener.simple_combo("a", delta_head.bind([10,0]));
+		listener.simple_combo("s", delta_head.bind([0,10]));
+		listener.simple_combo("d", delta_head.bind([-10,0]));
+		listener.simple_combo("k", function(){
+			qHead[0] = 0;
+			qHead[1] = 1;
+		});
+		// Enter the teleop mode
+		listener.simple_combo("enter", function(){
+			return util.shm('/fsm/Head/teleop', true);
+		});
+		listener.simple_combo("space", function(){
+			return util.shm('/shm/hcm/teleop/head', qHead);
+		});
+		listener.simple_combo("escape", function(){
+			return util.shm('/shm/hcm/teleop/head').then(function(q){
+				qHead[0] = q[0];
+				qHead[1] = q[1];
+			});
+		});
+	}
+
 	// Add the camera view and append
 	util.lhtml('/view/head_video.html').then(function(view) {
 		document.body = view;
-		return util.ljs('/VideoFeed.js');
+		return Promise.all([
+			util.ljs('/VideoFeed.js'),
+			util.ljs("/bc/Keypress/keypress.js")
+		]);
 	}).then(function(){
 		return util.shm('/streams/camera0');
 	}).then(function(port){
@@ -52,6 +85,8 @@
 		container.appendChild(lA_canvas);
 		lA_canvas.classList.toggle('nodisplay');
 		container.addEventListener('dblclick', toggle);
+	}).then(function(){
+		setTimeout(setup_keys, 0);
 	});
 
 	// LabelA WebWorker
